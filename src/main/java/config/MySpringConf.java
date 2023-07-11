8e7745e31589ccebc36aa554e0e7e6555c05f3f7
Arahorn34 <kocheryan90@mail.ru>
2023-07-11 03:41:02 +0300

    ддд

--- src/main/java/config/MySpringConf.java
+++ src/main/java/config/MySpringConf.java
@@ -1,2 +1,17 @@
-package config;public class MySpringConf {
+package config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringConf extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SprCon.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
 }
--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,2 +1,12 @@
-package controller;public class HelloController {
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
 }
